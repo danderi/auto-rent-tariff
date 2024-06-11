e02cbca14d50e9127b8c753e74cd5f52e407f362
danderi <anderi.daniel@gmail.com>
2024-06-11 11:15:09 -0400

    FLUX- Added the Render server URL

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -19,7 +19,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 					password: password
 				  };
 		
-				  const response = await fetch("http://127.0.0.1:5000/admin/users", {
+				  const response = await fetch("https://auto-rent-tariff-back.onrender.com/admin/users", {
 					method: "POST",
 					headers: {
 					  "Content-Type": "application/json"
