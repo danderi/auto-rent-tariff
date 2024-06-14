26cd4821c830b1a8bd73102186c1914afdd403dd
danderi <anderi.daniel@gmail.com>
2024-06-13 20:45:50 -0400

    FRONT - Se agrega vista de login y de register. tambien se actualiza el flux y el layout

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,47 +1,69 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			personas: ["Pedro","Maria"],
-			registerStatus: false
-		},
-		actions: {
+    return {
+        store: {
+            personas: ["Pedro", "Maria"],
+            registerStatus: false,
+            loginStatus: false
+        },
+        actions: {
+            exampleFunction: () => {
+                console.log("hola");
+                return;
+            },
 
-			exampleFunction: () => {
-                    console.log("hola")
-                    return
-			},
+            register: async (name, email, password) => {
+                try {
+                    const data = { name, email, password };
 
-			register: async (name, email, password) => {
-				try {
-				  const data = {
-					name: name,
-					email: email,
-					password: password
-				  };
-		
-				  const response = await fetch("https://auto-rent-tariff-back.onrender.com/admin/users", {
-					method: "POST",
-					headers: {
-					  "Content-Type": "application/json"
-					},
-					body: JSON.stringify(data)
-				  });
-		
-				  const statusCode = response.status;
-				  const responseData = await response.json();
-		
-				  if (statusCode === 201) {
-					setStore({ ...getStore(), registerStatus: true });
-				  }
-				  console.log(responseData)
-				  return responseData;
-				} catch (error) {
-				  console.error("Error:", error);
-				  throw error;
-				}
-		}
-		}
-	};
+                    const response = await fetch("https://auto-rent-tariff-back.onrender.com/admin/users", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify(data)
+                    });
+
+                    const statusCode = response.status;
+                    const responseData = await response.json();
+
+                    if (statusCode === 201) {
+                        setStore({ ...getStore(), registerStatus: true });
+                    }
+                    console.log(responseData);
+                    return responseData;
+                } catch (error) {
+                    console.error("Error:", error);
+                    throw error;
+                }
+            },
+
+            login: async (email, password) => {
+                try {
+                    const data = { email, password };
+
+                    const response = await fetch("https://auto-rent-tariff-back.onrender.com/admin/login", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify(data)
+                    });
+
+                    const statusCode = response.status;
+                    const responseData = await response.json();
+
+                    if (statusCode === 200) {
+                        setStore({ ...getStore(), loginStatus: true });
+                    }
+                    console.log(responseData);
+                    return responseData;
+                } catch (error) {
+                    console.error("Error:", error);
+                    throw error;
+                }
+            }
+        }
+    };
 };
 
 export default getState;
