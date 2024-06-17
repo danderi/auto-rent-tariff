42e27b46e35039dd6f9d40837676e19113e6c5a5
danderi <anderi.daniel@gmail.com>
2024-06-16 20:11:58 -0400

    Implemented user creation, login, and user listing endpoints with JWT authentication

--- src/api/app.py
+++ src/api/app.py
@@ -7,8 +7,12 @@ from admin_bp import admin_bp                       # Acá importamos rutas admi
 from public_bp import public_bp                     # Acá importamos rutas public
 from database import db                             # Acá importamos la base de datos inicializada
 from flask_cors import CORS
+from flask_migrate import Migrate
+from database import db
 
 app = Flask(__name__)
+
+migrate = Migrate(app, db)
 
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 # ENCRIPTACION JWT y BCRYPT-------
--- src/api/models.py
+++ src/api/models.py
@@ -6,3 +6,5 @@ class User(db.Model):
     last_name = db.Column(db.String(50))
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(255))
+
+
